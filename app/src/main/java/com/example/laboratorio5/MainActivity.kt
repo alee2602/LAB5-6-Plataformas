20d2a84eb0d6eaadff2ac6db299130226a7c5fd9
Mónica Salvatierra <sal22249@uvg.edu.gt>
2023-09-10 23:36:39 -0600

    Añadir correctamente activity de Favorites y cambios

--- app/src/main/java/com/example/laboratorio5/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio5/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
             //ConcertMainApp()
             //PlacesMainApp()
             //DetailsMainApp()
-            //ProfileMainApp()
+            ProfileMainApp()
         }
     }
 }

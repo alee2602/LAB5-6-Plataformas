38ea2582ebfc387618a8f1986fdc8635f806d0e9
Mónica Salvatierra <sal22249@uvg.edu.gt>
2023-09-10 23:50:30 -0600

    Actualizar MainActivity y se realizó el checkout de la nueva branch para realizar todos los commits de ahora en adelante ahí.

--- app/src/main/java/com/example/laboratorio5/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio5/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : ComponentActivity() {
             //PlacesMainApp()
             //DetailsMainApp()
             //ProfileMainApp()
+            //FavoritesMainApp()
         }
     }
 }
